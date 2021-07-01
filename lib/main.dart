531f65e6158cf12d5053f1a17ec9511e1240745e
vipuldamor87 <vipuldamor26@gmail.com>
2021-07-01 13:35:53 +0530

    decoration text properaties,scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,33 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('awesome app'),
       ),
-      body: Container(
-        child:Text('Hii Flutter'),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          width: 100,
+          height:100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.bottomRight,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            gradient: LinearGradient(colors:[Colors.purple,
+            Colors.yellow]),
+            boxShadow: [
+              BoxShadow(
+                color:Colors.grey,
+                blurRadius: 10,
+              )
+            ],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text('THis is a box',
+          textAlign: TextAlign.center,
+          style:TextStyle(
+            fontSize: 20,
+            color:Colors.white,
+            fontWeight: FontWeight.bold,
+          )),
+        ),
       ),
     );
   }
