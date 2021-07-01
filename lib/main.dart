5d489cdbf7eded517eada09731863f4606c33a62
vipuldamor87 <vipuldamor26@gmail.com>
2021-07-01 15:06:49 +0530

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -21,32 +21,36 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('awesome app'),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          width: 100,
-          height:100,
-          clipBehavior: Clip.antiAlias,
-          alignment: Alignment.bottomRight,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            gradient: LinearGradient(colors:[Colors.purple,
-            Colors.yellow]),
-            boxShadow: [
-              BoxShadow(
-                color:Colors.grey,
-                blurRadius: 10,
-              )
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Text('THis is a box',
-          textAlign: TextAlign.center,
-          style:TextStyle(
-            fontSize: 20,
-            color:Colors.white,
-            fontWeight: FontWeight.bold,
-          )),
+      body: Container(
+        width: 300,
+        color: Colors.teal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+          ],
         ),
       ),
     );
